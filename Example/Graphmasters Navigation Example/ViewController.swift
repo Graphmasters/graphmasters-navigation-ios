63e5fe2b454ab693647a43db3bda8787cbca40fd
Florian Herzog <florian.herzog@graphmasters.net>
2022-10-11 09:49:06 +0200

    #5 :recycle: refactored configuration

--- Example/Graphmasters Navigation Example/ViewController.swift
+++ Example/Graphmasters Navigation Example/ViewController.swift
@@ -1,41 +1,11 @@
-import UIKit
 import Mapbox
+import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var mapView: MGLMapView!
+    @IBOutlet var mapView: MGLMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.styleURL = URL(string: Configuration.mapStyleUrl)!
-    }
-}
-
-extension Configuration {
-    static var navigationApiUrl: String = try! Configuration.value(for: "NAVIGATION_API_URL")
-
-    static var navigationApiKey: String = try! Configuration.value(for: "NAVIGATION_API_KEY")
-
-    static var mapStyleUrl: String = try! Configuration.value(for: "MAP_STYLE_URL")
-}
-
-enum Configuration {
-    enum Error: Swift.Error {
-        case missingKey, invalidValue
-    }
-
-    static func value<T>(for key: String) throws -> T where T: LosslessStringConvertible {
-        guard let object = Bundle.main.object(forInfoDictionaryKey:key) else {
-            throw Error.missingKey
-        }
-
-        switch object {
-        case let value as T:
-            return value
-        case let string as String:
-            guard let value = T(string) else { fallthrough }
-            return value
-        default:
-            throw Error.invalidValue
-        }
     }
 }
