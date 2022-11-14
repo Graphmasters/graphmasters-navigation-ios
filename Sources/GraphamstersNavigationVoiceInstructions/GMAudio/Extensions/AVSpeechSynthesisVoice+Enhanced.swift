be00e303ed6d2583ad001fc5699d71b7125e8720
Florian Herzog <florian.herzog@graphmasters.net>
2022-11-14 14:12:34 +0100

    dynamically calculate voices

--- Sources/GraphamstersNavigationVoiceInstructions/GMAudio/Extensions/AVSpeechSynthesisVoice+Enhanced.swift
+++ Sources/GraphamstersNavigationVoiceInstructions/GMAudio/Extensions/AVSpeechSynthesisVoice+Enhanced.swift
@@ -1,17 +1,25 @@
 import AVFoundation
 
 public extension AVSpeechSynthesisVoice {
-    static var availableSynthesizerVoices: [AVSpeechSynthesisVoice] = AVSpeechSynthesisVoice.speechVoices()
-        .filter { $0.language == Locale.autoupdatingCurrent.languageCode }
-        .sorted { $0.identifier < $1.identifier }
+    static var availableSynthesizerVoices: [AVSpeechSynthesisVoice] {
+        AVSpeechSynthesisVoice.speechVoices()
+            .filter { $0.language == AVSpeechSynthesisVoice.currentLanguageCode() }
+            .sorted { $0.identifier < $1.identifier }
+    }
 
-    static var `default`: AVSpeechSynthesisVoice? = availableSynthesizerSiriVoices.first
-        ?? availableEnhancedVoices.first
-        ?? availableSynthesizerVoices.first
+    static var `default`: AVSpeechSynthesisVoice? {
+        availableSynthesizerSiriVoices.first
+            ?? availableEnhancedVoices.first
+            ?? availableSynthesizerVoices.first
+    }
 
-    static var availableEnhancedVoices: [AVSpeechSynthesisVoice] = availableSynthesizerVoices.filter { $0.quality == .enhanced }
+    static var availableEnhancedVoices: [AVSpeechSynthesisVoice] {
+        availableSynthesizerVoices.filter { $0.quality == .enhanced }
+    }
 
-    static var availableSynthesizerSiriVoices: [AVSpeechSynthesisVoice] = availableSynthesizerVoices.filter { $0.isSiriVoice }
+    static var availableSynthesizerSiriVoices: [AVSpeechSynthesisVoice] {
+        availableSynthesizerVoices.filter { $0.isSiriVoice }
+    }
 }
 
 public extension AVSpeechSynthesisVoice {
